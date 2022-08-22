f2443449f2453b9d1eac421be9e1cadb21c9fc0b
URK21CS1071 <109952128+URK21CS1071@users.noreply.github.com>
2022-08-22 14:20:20 +0530

    commit.2

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 class MyCircularQueue:
-    def _init_(self, size: int):
+    def __init__(self, size: int):
         self.size=size
         self.queue=[None]*size
         self.rear=-1
@@ -30,7 +30,7 @@ class MyCircularQueue:
                 return True
         else:
             return False
-                
+               
 
     def get_front(self) -> int:
         if(self.is_empty()==False):
@@ -82,3 +82,4 @@ for i in range(len(operations)):
         result.append(obj.is_empty())
 
 print(result)
+
